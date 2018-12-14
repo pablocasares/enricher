cd7ed95ddec9b6bc14ac0fc6641c7bd99be1ad03
Pablo Casares <pcasares@wizzie.io>
2018-12-14 11:10:47 +0100

    Add logo

--- service/src/main/java/io/wizzie/enricher/Enricher.java
+++ service/src/main/java/io/wizzie/enricher/Enricher.java
@@ -2,6 +2,7 @@ package io.wizzie.enricher;
 
 import io.wizzie.bootstrapper.builder.Config;
 import io.wizzie.enricher.builder.Builder;
+import io.wizzie.enricher.logo.LogoPrinter;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -9,6 +10,7 @@ public class Enricher {
     private static final Logger log = LoggerFactory.getLogger(Enricher.class);
 
     public static void main(String args[]) throws Exception {
+        LogoPrinter.PrintLogo();
         if (args.length == 1) {
             Config config = new Config(args[0]);
             Builder builder = new Builder(config.clone());
